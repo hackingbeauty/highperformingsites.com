008a8c59788a2e51fbb120121b8984270ffd5805
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-11-17 22:13:13 -0800

    created user model validations and tests

--- Gemfile
+++ Gemfile
@@ -22,7 +22,16 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 # gem 'sqlite3-ruby', :require => 'sqlite3'
 # gem 'aws-s3', :require => 'aws/s3'
 gem 'json'
-gem 'haml' 
+gem 'haml'
+
+group :development do
+  gem 'rspec-rails', '2.1.0'
+  gem 'annotate-models', '1.0.4'
+end
+
+group :test do
+end
+
 
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101031005535) do
+ActiveRecord::Schema.define(:version => 20101118061014) do
 
   create_table "grades", :force => true do |t|
     t.string   "yslow2_id"
@@ -96,6 +96,15 @@ ActiveRecord::Schema.define(:version => 20101031005535) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
   create_table "yslow2s", :force => true do |t|
     t.integer  "url_id"
