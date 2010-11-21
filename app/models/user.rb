70772404dabab23b6adad1afad7ef066593c717a
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-11-20 22:44:04 -0800

    first stab at encrypting passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,23 @@ class User < ActiveRecord::Base
   validates :password,  :presence     => true,
                         :confirmation => true,
                         :length       => { :within => 6..40 }
+                        
+  before_save :encrypt_password
+  
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    
+  end
+  
+  
+  private
+  
+    def encrypt_password
+      self.encrypted_password = encrypt(self.password)
+    end
+    
+    def encrypt(string)
+      string
+    end
 
 end
