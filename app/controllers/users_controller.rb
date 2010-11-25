f7472503178da987cb506f67eff9c8b9ae1b2f7e
Mark Muskardin <markmuskardin@mark-muskardins-macbook-pro.local>
2010-11-24 22:23:19 -0800

    completed signup form with tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
     @title = "Sign Up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
   end
 
 end
