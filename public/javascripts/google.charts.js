7232d06bb319710f8089f9cec690122c54cdeb0d
Mark Muskardin <markmuskardin@gmail.com>
2011-05-15 00:45:37 -0700

    creating trend graph for HTML

--- public/javascripts/google.charts.js
+++ public/javascripts/google.charts.js
@@ -1,42 +1,30 @@
 GoogleCharts = {
   val: "yada",
   init:function(){  
-    if($("#chart").length > 0 ){
-      //this.getData();
-      
+    if($("#chart").length > 0 ){      
       google.load('visualization', '1', {'packages':['corechart']});
       google.setOnLoadCallback(GoogleCharts.drawChart);
     }  
   },
   getData:function(){
-    var currentUrl = document.location.href;
-    var dataRecord = currentUrl.match(/url\/\d+/)[0].match(/\d+/)[0];
-    $.ajax({
-      url: '/get_trends?id='+dataRecord,
-      dataType: 'json',
-      success:function(res){
-        // console.log(res);
-        return res;
-      }
-    })
+    return JSON.parse($('#data').html().trim());
   },
   drawChart:function(){
-
+    var jsonData = GoogleCharts.getData();
+    
     // Create our data table.
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Size');
     data.addColumn('number', 'HTML');
-    data.addColumn('number', 'CSS');
-    data.addColumn('number', 'JavaScript');
-    
-    data.addRows([
-      ['Date', 3,2,1],
-      // ['Onions', 1],
-      // ['Olives', 1], 
-      // ['Zucchini', 1],
-      // ['Pepperoni', 2]
-    ]);
 
+    for(obj in jsonData){
+      if(jsonData.hasOwnProperty(obj)){
+        data.addRows([
+          [obj, jsonData[obj]]
+        ]);
+      }
+    }
+    
     // Instantiate and draw our chart, passing in some options.
     var chart = new google.visualization.LineChart(document.getElementById('chart'));
     chart.draw(data, { height: 440});
